c8e19847e677f97a63e8943b7edbb277f1a3c209
AnonymoCraft <anonymocrafteur@gmail.com>
2022-02-23 15:24:31 +0100

    Implement Conveyor Subsystem

--- src/main/cpp/subsystems/Conveyor.cpp
+++ src/main/cpp/subsystems/Conveyor.cpp
@@ -2,11 +2,52 @@
 
 Conveyor::Conveyor()
 {
-    m_ConveyorMotor.SetInverted(true);
+    m_ConveyorEntryMotor.ConfigFactoryDefault();
+    m_ConveyorMotor.RestoreFactoryDefaults();
+    m_ConveyorExitMotor.ConfigFactoryDefault();
+
+    m_ConveyorEntryMotor.SetNeutralMode(ctre::phoenix::motorcontrol::NeutralMode::Brake);
+    m_ConveyorMotor.SetIdleMode(rev::CANSparkMax::IdleMode::kBrake);
+    m_ConveyorExitMotor.SetNeutralMode(ctre::phoenix::motorcontrol::NeutralMode::Brake);
+
+    m_ConveyorEntryMotor.SetInverted(false);
+    m_ConveyorMotor.SetInverted(false);
+    m_ConveyorExitMotor.SetInverted(false);
 }
 
-void Conveyor::ActiveMotor() { m_ConveyorMotor.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, CONVEYOR_MOTOR_SPEED); }
+void Conveyor::StopAllMotors()
+{
+    m_ConveyorEntryMotor.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, 0.0);
+    m_ConveyorMotor.Set(0);
+    m_ConveyorExitMotor.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, 0.0);
+}
 
-void Conveyor::UnblockMotor() { m_ConveyorMotor.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, -CONVEYOR_MOTOR_SPEED); }
+void Conveyor::ActiveEntryMotor(double speed)
+{
+    m_ConveyorEntryMotor.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, speed);
+}
 
-void Conveyor::StopMotor() { m_ConveyorMotor.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, 0.0); }
+void Conveyor::ActiveExitMotor(double speed)
+{
+    m_ConveyorExitMotor.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, speed);
+}
+
+void Conveyor::ActiveConveyorMotor(double speed)
+{
+    m_ConveyorMotor.Set(speed);
+}
+
+void Conveyor::StopEntryMotor()
+{
+    m_ConveyorEntryMotor.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, 0);
+}
+
+void Conveyor::StopExitMotor()
+{
+    m_ConveyorExitMotor.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, 0);
+}
+
+void Conveyor::StopConveyorMotor()
+{
+    m_ConveyorMotor.Set(0);
+}
--- src/main/include/subsystems/Conveyor.h
+++ src/main/include/subsystems/Conveyor.h
@@ -6,6 +6,7 @@
 
 #include <frc2/command/SubsystemBase.h>
 #include <ctre/phoenix/motorcontrol/can/VictorSPX.h>
+#include <rev/CANSparkMax.h>
 
 #include "Constants.h"
 
@@ -13,10 +14,16 @@ class Conveyor : public frc2::SubsystemBase
 {
 public:
     Conveyor();
-    void ActiveMotor();
-    void UnblockMotor();
-    void StopMotor();
+    void StopAllMotors();
+    void ActiveEntryMotor(double speed);
+    void ActiveExitMotor(double speed);
+    void ActiveConveyorMotor(double speed);
+    void StopEntryMotor();
+    void StopExitMotor();
+    void StopConveyorMotor();
 
 private:
-    ctre::phoenix::motorcontrol::can::VictorSPX m_ConveyorMotor{CONVEYOR_MOTOR_ID};
+    ctre::phoenix::motorcontrol::can::VictorSPX m_ConveyorEntryMotor{CONVEYOR_MOTOR_ID};
+    ctre::phoenix::motorcontrol::can::VictorSPX m_ConveyorExitMotor{CONVEYOR_EXIT_MOTOR_ID};
+    rev::CANSparkMax m_ConveyorMotor{CONVEYOR_MOTOR_ID, rev::CANSparkMax::MotorType::kBrushless};
 };
