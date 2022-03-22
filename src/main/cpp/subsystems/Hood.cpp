02fbdfd550cd3255333a8bb1dcf04f8442167948
mimi980 <90112270+mimi980@users.noreply.github.com>
2022-03-22 22:43:09 +0100

    implement hood profiledpid

--- src/main/cpp/subsystems/Hood.cpp
+++ src/main/cpp/subsystems/Hood.cpp
@@ -12,25 +12,45 @@
 Hood::Hood()
 {
     m_encoderHood.SetDistancePerRotation(-(58 / 4.2));
-    m_pidController.SetSetpoint(0.0);
+
+    Enable();
+    SetSetpoint(0.0);
 
     m_HoodMotor.SetIdleMode(rev::CANSparkMax::IdleMode::kBrake);
     m_HoodMotor.SetInverted(true);
-    m_pidController.SetIntegratorRange(-HOOD_PID_INTEGRATOR_RANGE, HOOD_PID_INTEGRATOR_RANGE);
-    m_pidController.SetTolerance(HOOD_PID_TOLERANCE);
-
+    // m_pidcontroller.SetIntegratorRange(-HOOD_PID_INTEGRATOR_RANGE, HOOD_PID_INTEGRATOR_RANGE);
+    // m_pidcontroller.SetTolerance(HOOD_PID_TOLERANCE);
+    m_hallSecurity.setInverted(true); // TODO verifier ca mais je pense que c'est vrai
     ResetEncoders();
 
 #if SHUFFLEBOARD_DEBUG
-    m_entry_HoodPosition = frc::Shuffleboard::GetTab("Hood DEBUG").Add("Hood Position", GetEncoderDistance()).GetEntry();
-    m_entry_HoodSetpoint = frc::Shuffleboard::GetTab("Hood DEBUG").Add("Hood Setpoint", m_pidController.GetSetpoint()).GetEntry();
-    m_entry_HoodError = frc::Shuffleboard::GetTab("Hood DEBUG").Add("Hood Error", m_pidController.GetPositionError()).GetEntry();
-    m_entry_HoodOutput = frc::Shuffleboard::GetTab("Hood DEBUG").Add("Hood Output", 0.0).GetEntry();
-    m_entry_HoodState = frc::Shuffleboard::GetTab("Hood DEBUG").Add("Hood State", StateToString(m_state)).GetEntry();
+    // m_entry_HoodPosition = frc::Shuffleboard::GetTab("Hood DEBUG").Add("Hood Position", GetMeasurement()).GetEntry();
+    // m_entry_HoodSetpoint = frc::Shuffleboard::GetTab("Hood DEBUG").Add("Hood Setpoint", GetSetpoint()).GetEntry();
+    // m_entry_HoodError = frc::Shuffleboard::GetTab("Hood DEBUG").Add("Hood Error", m_pidcontroller.GetPositionError()).GetEntry();
+    // m_entry_HoodOutput = frc::Shuffleboard::GetTab("Hood DEBUG").Add("Hood Output", 0.0).GetEntry();
+    // m_entry_HoodState = frc::Shuffleboard::GetTab("Hood DEBUG").Add("Hood State", StateToString(m_state)).GetEntry();
 #endif
 }
 
-bool Hood::MagnetDetected() { return !m_SensorHall.Get(); }
+void Hood::Enable()
+{
+    m_state = Hood::state::Enabled;
+}
+
+void Hood::Disable()
+{
+    m_state = Hood::state::Disabled;
+}
+
+void Hood::SetSetpoint(double setpoint)
+{
+    m_setPoint = setpoint;
+}
+
+double Hood::GetSetpoint()
+{
+    return m_setPoint;
+}
 
 void Hood::ResetEncoders()
 {
@@ -38,131 +58,78 @@ void Hood::ResetEncoders()
     m_HoodMotor.GetEncoder().SetPosition(0.0);
 }
 
+double Hood::GetMeasurement()
+{
+    return m_encoderHood.GetDistance();
+}
+
 void Hood::UseOutput(double output, double setpoint)
 {
-    m_Position = GetEncoderDistance();
-    m_DeltaPosition = m_Position - m_PositionBefore;
-    if (m_DeltaPosition > -0.3 && m_DeltaPosition < 0.3)
-        m_DeltaPosition = 0;
-    m_PositionBefore = m_Position;
     switch (m_state)
     {
-    case Hood::state::Init:
-        m_Position = m_PositionBefore = GetEncoderDistance();
-        if (MagnetDetected())
+    case Hood::state::WaitingEncoder:
+        if (m_encoderHood.IsConnected())
         {
-            m_state = Hood::state::haut_Direction;
+            ResetEncoders();
+            m_state = Hood::state::Init;
+        }
+        break;
+    case Hood::state::Init:
+        if (m_hallSecurity.MagnetDetected())
+        {
+            m_state = Hood::state::Disabled;
             ResetEncoders();
             SetSetpoint(0.0);
         }
         else
         {
             SetSetpoint(-60.0);
-            m_HoodMotor.Set(std::clamp(output, -HOOD_INIT_MAX_SPEED, HOOD_INIT_MAX_SPEED));
-        }
-        break;
-    case Hood::state::bas_Direction:
-        if (output < 0) // si le pid renvoie <0 mettre moteur vitesse normal
-        {
-            // SetSetpoint((frc::SmartDashboard::GetNumber("Setpoint m_hood", 0.0)));
-            m_HoodMotor.Set(std::clamp(output, -HOOD_MAX_SPEED, HOOD_MAX_SPEED));
-        }
-        else
-        { // sinon mettre le hood a 0.0
-            m_HoodMotor.Set(0.0);
-        }
-        if (!MagnetDetected())
-        {                                        // si on ne détecte plus
-            m_state = Hood::state::bh_Direction; // mettre state à bh_direction
-        }
-        break;
-
-    case Hood::state::haut_Direction:
-        if (output > 0) // si le pid renvoie >0 mettre moteur vitesse normal
-        {
-            // SetSetpoint((frc::SmartDashboard::GetNumber("Setpoint m_hood", 0.0)));
-            m_HoodMotor.Set(std::clamp(output, -HOOD_MAX_SPEED, HOOD_MAX_SPEED));
-        }
-        else
-        { // sinon mettre vitesse à 0
-            m_HoodMotor.Set(0.0);
-        }
-        if (!MagnetDetected())
-        {                                        // si on ne détecte plus
-            m_state = Hood::state::bh_Direction; // mettre state à bh_direction
-        }
-
-        break;
-
-    case Hood::state::bh_Direction:
-        if (MagnetDetected())
-        { // si on détecte un aimant
-            if (m_DeltaPosition < 0)
-            { // si on va en bas
-                if (output > 0)
-                { // si joystick renvoie >0 mettre moteur vitesse normal
-                    // SetSetpoint((frc::SmartDashboard::GetNumber("Setpoint m_hood", 0.0)));
-                    m_HoodMotor.Set(std::clamp(output, -HOOD_MAX_SPEED, HOOD_MAX_SPEED));
-                }
-                else
-                { // sinon mettre le poid du Hood
-                    m_HoodMotor.Set(0.0);
-                    // est ce que le hood a une barre qui le maintien (mettre a O ?)
-                }
-                m_state = Hood::state::haut_Direction; // mettre state à haut
+            if (!m_hallSecurity.ShouldIStop(GetMeasurement(), wpi::sgn(output)))
+            {
+                m_HoodMotor.Set(std::clamp(output, -0.1, 0.1));
             }
             else
-            { // sinon si on vas en haut
-                if (output < 0)
-                { // si joystick renvoie <0 mettre moteur vitesse normal
-                    // SetSetpoint((frc::SmartDashboard::GetNumber("Setpoint m_hood", 0.0)));
-                    m_HoodMotor.Set(0.0);
-                }
-                else
-                { // sinon mettre le poid du Hood
-                    m_HoodMotor.Set(std::clamp(output, -HOOD_MAX_SPEED, HOOD_MAX_SPEED));
-                }
-                m_state = Hood::state::bas_Direction; // mettre state à bas
+            {
+                m_HoodMotor.Set(0.0);
             }
         }
-        else
-        { // sinon pas d'aimant mettre vitesse normal
-            // SetSetpoint((frc::SmartDashboard::GetNumber("Setpoint m_hood", 0.0)));
-            m_HoodMotor.Set(std::clamp(output, -HOOD_MAX_SPEED, HOOD_MAX_SPEED));
-        }
-
         break;
 
+    case Hood::state::Enabled:
+        if (m_hallSecurity.ShouldIStop(GetMeasurement(), wpi::sgn(output)))
+        {
+            m_HoodMotor.Set(output);
+        }
+        else
+        {
+            m_HoodMotor.Set(0.0);
+        }
+        break;
+    case Hood::state::Disabled:
+        m_HoodMotor.Set(0.0);
+        break;
     default:
         break;
     }
-}
-
-double Hood::GetEncoderDistance()
-{
-    return m_encoderHood.GetDistance();
+    frc::SmartDashboard::PutNumber("outputHood", output);
 }
 
 void Hood::Periodic()
 {
-    double output = m_pidController.Calculate(GetEncoderDistance());
-    UseOutput(output, m_pidController.GetSetpoint());
+    double output = m_pidcontroller.Calculate(units::degree_t{GetMeasurement()}, units::degree_t{m_setPoint});
+    UseOutput(output, GetSetpoint());
 
 #if SHUFFLEBOARD_DEBUG
-    m_entry_HoodPosition.SetDouble(GetEncoderDistance());
-    m_entry_HoodSetpoint.SetDouble(m_pidController.GetSetpoint());
-    m_entry_HoodError.SetDouble(m_pidController.GetPositionError());
-    m_entry_HoodOutput.SetDouble(output);
-    m_entry_HoodState.SetString(StateToString(m_state));
+    // m_entry_HoodPosition.SetDouble(GetMeasurement());
+    // m_entry_HoodSetpoint.SetDouble(GetSetpoint());
+    // m_entry_HoodError.SetDouble(m_pidcontroller.GetPositionError());
+    // m_entry_HoodOutput.SetDouble(output);
+    // m_entry_HoodState.SetString(StateToString(m_state));
 #endif
 }
 
-void Hood::SetSetpoint(double setpoint)
-{
-    m_pidController.SetSetpoint(setpoint);
-}
-
 // Convert state number to name
+
 std::string Hood::StateToString(int state)
 {
     switch (state)
--- src/main/include/subsystems/Hood.h
+++ src/main/include/subsystems/Hood.h
@@ -4,48 +4,61 @@
 
 #pragma once
 
-#include <frc/controller/PIDController.h>
 #include <frc2/command/SubsystemBase.h>
+#include <frc2/command/ProfiledPIDCommand.h>
 #include <frc/DutyCycleEncoder.h>
 #include <rev/CANSparkMax.h>
 #include <iostream>
 #include <frc/DigitalInput.h>
+#include <units/angle.h>
+#include <frc/smartdashboard/SmartDashboard.h>
+#include <frc/shuffleboard/Shuffleboard.h>
 #include "Constants.h"
-#include <networktables/NetworkTableEntry.h>
+
+#include "lib/HallSecurity.h"
 
 class Hood : public frc2::SubsystemBase
 {
 public:
   Hood();
-  double GetEncoderDistance();
-  void ResetEncoders();
-  bool MagnetDetected();
+
+  double GetMeasurement();
+
+  void Periodic() override;
+
   void SetSetpoint(double setpoint);
-  void Periodic();
+
+  double GetSetpoint();
+
+  void Enable();
+
+  void Disable();
+
+  void ResetEncoders();
+
+  void UseOutput(double output, double setpoint);
+
   static std::string StateToString(int state);
 
-  double m_DeltaPosition;
-  double m_Position;
-  double m_PositionBefore;
-
+private:
   enum state
   {
+    WaitingEncoder,
     Init,
-    haut_Direction,
-    bas_Direction,
-    bh_Direction,
-    StopSecure
-    // Ready,
+    Enabled,
+    Disabled
   };
-  Hood::state m_state = Hood::state::Init;
+  Hood::state m_state = Hood::state::WaitingEncoder;
 
-private:
-  void UseOutput(double output, double setpoint);
+  frc::ProfiledPIDController<units::degree> m_pidcontroller{
+      HOOD_PID_P, HOOD_PID_I, HOOD_PID_D,
+      frc::TrapezoidProfile<units::degree>::Constraints{5_deg / 1_s, 10_deg / (1_s * 1_s)}};
   frc::DutyCycleEncoder m_encoderHood{HOOD_ENCODER_ID};
-  frc::DigitalInput m_SensorHall{HOOD_SENSOR_HALL_ID};
 
   rev::CANSparkMax m_HoodMotor{1, rev::CANSparkMax::MotorType::kBrushless};
-  frc2::PIDController m_pidController{HOOD_PID_P, HOOD_PID_I, HOOD_PID_D};
+  HallSecurity m_hallSecurity{HOOD_SENSOR_HALL_ID, 0.3};
+
+  double m_setPoint = 0;
 
 #if SHUFFLEBOARD_DEBUG
   nt::NetworkTableEntry m_entry_HoodPosition;
