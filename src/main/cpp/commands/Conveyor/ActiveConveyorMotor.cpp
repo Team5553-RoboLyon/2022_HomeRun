d1c12c2491ca6e28f41be939e5770008a8cb06ca
mimi980 <90112270+mimi980@users.noreply.github.com>
2022-03-23 17:36:24 +0100

    add command conveyor but not finish

---
+++ src/main/cpp/commands/Conveyor/ActiveConveyorMotor.cpp
@@ -0,0 +1,26 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "commands/Conveyor/ActiveConveyorMotor.h"
+
+ActiveConveyorMotor::ActiveConveyorMotor(Conveyor *pconveyor)
+    : m_pConveyor(pconveyor)
+{
+  AddRequirements(m_pConveyor);
+}
+
+// Called when the command is initially scheduled.
+void ActiveConveyorMotor::Initialize() {}
+
+// Called repeatedly when this Command is scheduled to run
+void ActiveConveyorMotor::Execute() {}
+
+// Called once the command ends or is interrupted.
+void ActiveConveyorMotor::End(bool interrupted) {}
+
+// Returns true when the command should end.
+bool ActiveConveyorMotor::IsFinished()
+{
+  return false;
+}
---
+++ src/main/cpp/commands/Conveyor/UnblockFeederMotor.cpp
@@ -0,0 +1,27 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "commands/Conveyor/UnblockFeederMotor.h"
+
+UnblockFeederMotor::UnblockFeederMotor(Conveyor *pconveyor)
+    : m_pConveyor(pconveyor)
+
+{
+  AddRequirements(m_pConveyor);
+}
+
+// Called when the command is initially scheduled.
+void UnblockFeederMotor::Initialize() {}
+
+// Called repeatedly when this Command is scheduled to run
+void UnblockFeederMotor::Execute() {}
+
+// Called once the command ends or is interrupted.
+void UnblockFeederMotor::End(bool interrupted) {}
+
+// Returns true when the command should end.
+bool UnblockFeederMotor::IsFinished()
+{
+  return false;
+}
