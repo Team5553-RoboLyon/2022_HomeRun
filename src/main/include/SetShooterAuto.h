21bf2b1f90bc5daf4360aeb440daae598d53cf46
Eymeric <hatchchien@protonmail.com>
2022-03-30 20:58:13 +0200

    add python sript to convert csv data to hardcoded data

--- src/main/include/SetShooterAuto.h
+++ src/main/include/SetShooterAuto.h
@@ -12,7 +12,8 @@
 
 double shooterDataTable[2][3] = {
     {1.3, 1.4, 1.5},
-    {1.6, 1.7, 1.8}};
+    {1.6, 1.7, 1.8},
+};
 
 class SetShooterAuto
     : public frc2::CommandHelper<frc2::InstantCommand,
